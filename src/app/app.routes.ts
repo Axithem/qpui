c76de8c2b641598bd1520ccf54788b4db3453c93
Proxyfil <piloupilou200@gmail.com>
2026-03-14 11:48:02 +0100

    feat: Updated service to organize it and allow routing, created default room page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RoomPage } from './pages/room/room';
+import { DefaultPage } from './pages/default/default'
 
-export const routes: Routes = [];
+// Define each route in the app, for the moment 2 will be enough
+// Previous app will still be available in route "/"
+export const routes: Routes = [
+    { path: '', component: DefaultPage},
+    { path: 'room', component: RoomPage },
+];
